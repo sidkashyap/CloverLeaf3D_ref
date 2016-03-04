1160359276f832a41d32c60edace7a186abfb05f
sid <sidharth.n.kashyap@gmail.com>
2016-03-04 16:54:22 +0000

    script sweep

--- searchSpaceSweep.sh
+++ searchSpaceSweep.sh
@@ -12,14 +12,13 @@ do
         for (( i=$z;i>=2; ))
         do 
 	        echo  "$i $k $j "
-            i=`expr $i / 2`
             
-            sed -i "s#BLOCK_SIZE_x=.*#BLOCK_SIZE_x=${i}#g" clover.in
-            sed -i "s#BLOCK_SIZE_y=.*#BLOCK_SIZE_y=${k}#g" clover.in
-            sed -i "s#BLOCK_SIZE_z=.*#BLOCK_SIZE_z=${i}#g" clover.in
+            sed -i "s#block_size_x=.*#block_size_x=${i}#g" clover.in
+            sed -i "s#block_size_y=.*#block_size_y=${k}#g" clover.in
+            sed -i "s#block_size_z=.*#block_size_z=${j}#g" clover.in
             ./clover_leaf 
             cp clover.out clover.out.${i}.${k}.${j}
-            
+            i=`expr $i / 2`
         done
         k=`expr $k / 2`
     done
